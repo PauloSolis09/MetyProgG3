292f062ca8d2e46d33eaef7e6d5bb871dcbe9645
Laboratorio C205 <labc205@uam.net.ni>
2024-08-19 08:16:49 -0600

    sesion 1

---
+++ ps/HolaMundo/HolaMundo/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace saludo
+{
+    internal class Program
+    {
+        static void main(string[] args)
+        {
+            //leer el nombre y saludar 
+            string nombre;
+            string apellido;
+            Console.Write("dime tu nombre");
+            nombre = Console.ReadLine();
+            Console.Write("hola +{nombre} {apellido}, mucho gusto");
+        }
+    }
+}
